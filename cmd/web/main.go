f857b8d51a7530122b56eb2b2535a0c27c010dd2
AlejoGarat <alejogaratd@gmail.com>
2023-05-16 15:54:53 -0300

    Chapter 5.2. Connection to PostreSQL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 
@@ -21,6 +24,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 func main() {
@@ -28,6 +34,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -45,6 +52,12 @@ func main() {
 	// helloWorldRepo := repo.NewSqlxRepo(nil)
 	// helloWorldLogic := logic.NewHelloWorldLogic(helloWorldRepo)
 	// helloWorldHandler := handlers.NewHelloWorldHandler(helloWorldLogic)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	r := gin.Default()
 	r.NoRoute(gin.HandlerFunc(httphelpers.StatusNotFoundResponse))
@@ -75,4 +88,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
