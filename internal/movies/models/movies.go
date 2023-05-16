d3fd23bc994a193d69f229523dbfaabc309b47a0
AlejoGarat <alejogaratd@gmail.com>
2023-05-16 12:47:27 -0300

    Chapter 3.5. Advanced JSON Customization

--- internal/movies/models/movies.go
+++ internal/movies/models/movies.go
@@ -2,14 +2,16 @@ package data
 
 import (
 	"time"
+
+	"greenlight/internal/models"
 )
 
 type Movie struct {
-	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"-"` // omit from output
-	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"`
-	Genres    []string  `json:"genres"`
-	Version   int32     `json:"version"`
+	ID        int64          `json:"id"`
+	CreatedAt time.Time      `json:"-"` // omit from output
+	Title     string         `json:"title"`
+	Year      int32          `json:"year"`
+	Runtime   models.Runtime `json:"runtime, omitempty"`
+	Genres    []string       `json:"genres"`
+	Version   int32          `json:"version"`
 }
