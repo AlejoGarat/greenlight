0d4ac427c90b8104c47111459934491b88db47be
AlejoGarat <alejogaratd@gmail.com>
2023-05-19 14:53:03 -0300

    Chapter 11.2. IP-based Rate Limiting

--- pkg/middlewares/ratelimit_middleware.go
+++ pkg/middlewares/ratelimit_middleware.go
@@ -1,22 +1,66 @@
 package middlewares
 
 import (
+	"net"
+	"sync"
+	"time"
+
 	"greenlight/pkg/httphelpers"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/time/rate"
 )
 
+type client struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
 func RateLimit() gin.HandlerFunc {
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			mu.Unlock()
+		}
+	}()
 
 	return func(c *gin.Context) {
-		defer func() {
-			if !limiter.Allow() {
-				httphelpers.RateLimitExceededResponse(c)
-				return
-			}
-		}()
+		ip, _, err := net.SplitHostPort(c.Request.RemoteAddr)
+		if err != nil {
+			httphelpers.StatusInternalServerErrorResponse(c, err)
+			c.Abort()
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			httphelpers.RateLimitExceededResponse(c)
+			c.Abort()
+			return
+		}
+
+		mu.Unlock()
 		c.Next()
 	}
 }
