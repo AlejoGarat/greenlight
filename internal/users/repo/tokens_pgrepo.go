0ac8c7c60a1e9e2fe44e2e011e2eadd2a0a810bd
AlejoGarat <alejogaratd@gmail.com>
2023-05-22 15:41:57 -0300

    Chapter 15.2. Creating Secure Activation tokens

---
+++ internal/users/repo/tokens_pgrepo.go
@@ -0,0 +1,51 @@
+package repo
+
+import (
+	"context"
+	"time"
+
+	models "greenlight/internal/users/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type tokenRepo struct {
+	DB *sqlx.DB
+}
+
+func New(db *sqlx.DB) *tokenRepo {
+	return &tokenRepo{
+		DB: db,
+	}
+}
+
+func (r tokenRepo) Insert(ctx context.Context, userID int64, ttl time.Duration, scope string) (models.Token, error) {
+	token, err := models.GenerateToken(userID, ttl, scope)
+	if err != nil {
+		return models.Token{}, err
+	}
+
+	query := `
+        INSERT INTO tokens (hash, user_id, expiry, scope) 
+        VALUES ($1, $2, $3, $4)`
+
+	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	_, err = r.DB.ExecContext(ctx, query, args...)
+	return models.Token{}, err
+}
+
+func (r tokenRepo) DeleteAllForUser(ctx context.Context, scope string, userID int64) error {
+	query := `
+        DELETE FROM tokens 
+        WHERE scope = $1 AND user_id = $2`
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	_, err := r.DB.ExecContext(ctx, query, scope, userID)
+	return err
+}
---
+++ internal/users/repo/user_pgrepo.go
@@ -0,0 +1,109 @@
+package repo
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"greenlight/internal/users/models"
+	"greenlight/internal/users/repoerrors"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type userRepo struct {
+	DB *sqlx.DB
+}
+
+func NewMovieRepo(db *sqlx.DB) *userRepo {
+	return &userRepo{
+		DB: db,
+	}
+}
+
+func (r userRepo) Insert(ctx context.Context, user models.User) (models.User, error) {
+	query := `
+	INSERT INTO users (name, email, password_hash, activated)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version`
+
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Password.Hash,
+		user.Activated,
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	err := r.DB.GetContext(ctx, &user, query, args...)
+	if err != nil {
+		switch {
+		case err.Error() == "pq: duplicate key value violates unique constraint \"users_email_key\"":
+			return models.User{}, repoerrors.ErrDuplicateEmail
+		default:
+			return models.User{}, err
+		}
+	}
+
+	return user, nil
+}
+
+func (r userRepo) GetByEmail(ctx context.Context, email string) (models.User, error) {
+	query := `
+	SELECT id, created_at, name, email, password_hash, activated, version
+	FROM users
+	WHERE email = $1`
+
+	var user models.User
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	err := r.DB.GetContext(ctx, &user, query)
+	if err != nil {
+		switch {
+		case errors.Is(err, repoerrors.ErrNoRows):
+			return models.User{}, repoerrors.ErrRecordNotFound
+		default:
+			return models.User{}, err
+		}
+	}
+
+	return user, nil
+}
+
+func (r userRepo) Update(ctx context.Context, user models.User) (models.User, error) {
+	query := `
+	UPDATE users 
+	SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+	WHERE id = $5 AND version = $6
+	RETURNING version`
+
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Password.Hash,
+		user.Activated,
+		user.ID,
+		user.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	err := r.DB.GetContext(ctx, &user, query, args...)
+	if err != nil {
+		switch {
+		case err.Error() == "pq: duplicate key value violates unique constraint \"users_email_key\"":
+			return models.User{}, repoerrors.ErrDuplicateEmail
+		case errors.Is(err, repoerrors.ErrNoRows):
+			return models.User{}, repoerrors.ErrEditConflict
+		default:
+			return models.User{}, err
+		}
+	}
+
+	return user, nil
+}
