377a30a15744f7ace76a5bc656e6afdddfece053
AlejoGarat <alejogaratd@gmail.com>
2023-05-24 15:18:14 -0300

    Chapter 19.2. Creating Custom Metrics

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"expvar"
 	"flag"
 	"os"
+	"runtime"
 	"strings"
 	"time"
 
@@ -161,6 +163,17 @@ func main() {
 		userRoutes.MakeRoutes(v1, usersHandler, tokensHandler)
 		metricsRoutes.MakeRoutes(v1)
 	}
+
+	expvar.NewString("version").Set(version)
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	info := info{
 		cfg:                &cfg,
